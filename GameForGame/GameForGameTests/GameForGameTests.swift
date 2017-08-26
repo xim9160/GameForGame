d863fd7d892af52e590543d155766969e56818eb
闹钟 <281016974@qq.com>
2017-08-26 21:50:53 +0800

    Start-creat project

---
+++ GameForGame/GameForGameTests/GameForGameTests.swift
@@ -0,0 +1,36 @@
+//
+//  GameForGameTests.swift
+//  GameForGameTests
+//
+//  Created by 肖冯敏 on 2017/8/26.
+//  Copyright (c) 2017 oclcok.xim. All rights reserved.
+//
+
+import XCTest
+@testable import GameForGame
+
+class GameForGameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
